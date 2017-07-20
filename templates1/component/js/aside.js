d02660dece116330af821b97f7d49d7cb3c533ac
杨文智 <yangwenzhi@yangwenzhideMacBook-Pro.local>
2017-07-20 13:11:15 +0800

    d

--- templates1/component/js/aside.js
+++ templates1/component/js/aside.js
@@ -1,1 +1,8 @@
 // tpl
+$(".E-widget").each(function(){vv.tabs.getAttr(this)});
+vv.ajax.init();
+vv.event.init();
+
+$('#clickBtn').on('click', function(){
+	vv.tabs.trigger(this);
+})
--- templates1/js/index.js
+++ templates1/js/index.js
@@ -1,5 +1,9 @@
 //##include('../../../../vv-base/js/base.js');
 //##include('../../../../vv-base/js/doT.js');
-//##include('../component/js/aside.js');
 
-console.log('create success');
+var vv = window.vv || {};   
+//##include('../../../../vv-base/js/utils/storage.js');
+//##include('../../../../vv-base/js/utils/ajax.js');
+//##include('../../../../vv-base/js/utils/tabs.js');
+//##include('../../../../vv-base/js/utils/follow_event.js');
+//##include('../component/js/aside.js');
