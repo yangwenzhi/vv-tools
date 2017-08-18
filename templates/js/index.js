0f75237df6ced815709a5e7a1d0637d58d346948
Your Name <you@example.com>
2017-08-18 16:29:38 +0800

    download

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,5 +1,6 @@
 //##include('../../../vv-base/js/base.js');
 //##include('../../../vv-base/js/doT.js');
+//##include('../../../vv-base/js/download.js');
 
 //##include('../../../vv-base/js/utils/storage.js');
 //##include('../../../vv-base/js/utils/ajax.js');
